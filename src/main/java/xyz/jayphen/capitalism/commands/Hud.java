789a509af67c4669deb85be2359cc82357d34944
Jaycadox <68955944+Jaycadox@users.noreply.github.com>
2022-07-03 13:26:42 +1000

    - /hud command with scoreboard showcasing information

---
+++ src/main/java/xyz/jayphen/capitalism/commands/Hud.java
@@ -0,0 +1,100 @@
+package xyz.jayphen.capitalism.commands;
+
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.jetbrains.annotations.NotNull;
+import xyz.jayphen.capitalism.Capitalism;
+import xyz.jayphen.capitalism.claims.ClaimManager;
+import xyz.jayphen.capitalism.claims.region.RegionManager;
+import xyz.jayphen.capitalism.database.player.DatabasePlayer;
+import xyz.jayphen.capitalism.lang.NumberFormatter;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Hud implements CommandExecutor {
+	
+	private static final HashMap<UUID, ScoreState> states     = new HashMap<>();
+	private static final LegacyComponentSerializer serializer = LegacyComponentSerializer.builder().build();
+	private static final Scoreboard                empty      = Bukkit.getScoreboardManager().getNewScoreboard();
+	
+	public Hud() {
+		Capitalism.plugin.getServer().getScheduler().runTaskTimer(Capitalism.plugin, this::tick, 20, 20);
+		
+		
+	}
+	
+	private void tick() {
+		states.forEach((u, s) -> {
+			var player = Bukkit.getPlayer(u);
+			if (player == null) return;
+			update(player, s.o);
+		});
+	}
+	
+	private void update(Player p, Objective o) {
+		states.get(p.getUniqueId()).board.getObjectives().forEach(Objective::unregister);
+		o = states.get(p.getUniqueId()).board.registerNewObjective(p.getUniqueId() + "-obj", "dummy",
+		                                                           Component.text("Capitalism HUD", TextColor.color(0xeeeeee))
+		);
+		String line1 = serializer.serialize(Component.text("Region > ", TextColor.color(0xddddff))
+				                                    .append(Component.text(
+						                                    RegionManager.getRegion(p.getLocation()).toString().toUpperCase(),
+						                                    NamedTextColor.YELLOW
+				                                    )));
+		o.getScore(line1).setScore(1);
+		
+		String line2 = serializer.serialize(Component.text("Balance > ", TextColor.color(0xddddff))
+				                                    .append(Component.text(
+						                                    "$" + NumberFormatter.addCommas(DatabasePlayer.from(p).getMoneySafe()),
+						                                    NamedTextColor.YELLOW
+				                                    )));
+		o.getScore(line2).setScore(0);
+		var claimOwner = "Not inside claim";
+		var claim      = ClaimManager.getCachedClaim(p.getLocation()).orElse(null);
+		if (claim != null) {
+			claimOwner = Bukkit.getOfflinePlayer(UUID.fromString(claim.owner)).getName();
+		}
+		String line3 = serializer.serialize(Component.text("Claim > ", TextColor.color(0xddddff))
+				                                    .append(Component.text(claimOwner, NamedTextColor.YELLOW)));
+		o.getScore(line3).setScore(2);
+		
+		o.setDisplaySlot(DisplaySlot.SIDEBAR);
+		
+		
+		p.setScoreboard(!states.get(p.getUniqueId()).open[0] ? empty : states.get(p.getUniqueId()).board);
+	}
+	
+	private void generateScoreboard(Player p) {
+		var board = Bukkit.getScoreboardManager().getNewScoreboard();
+		var objective = board.registerNewObjective(p.getUniqueId() + "-obj", "dummy",
+		                                           Component.text("Capitalism HUD", TextColor.color(0xeeeeee))
+		);
+		states.put(p.getUniqueId(), new ScoreState(board, objective, new Boolean[] { false }));
+		update(p, objective);
+	}
+	
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+		if (!( sender instanceof Player p )) return true;
+		if (!states.containsKey(p.getUniqueId())) {
+			generateScoreboard(p);
+		}
+		states.get(p.getUniqueId()).open[0] = !states.get(p.getUniqueId()).open[0];
+		
+		return true;
+	}
+	
+	private record ScoreState(Scoreboard board, Objective o, Boolean[] open) {}
+}
